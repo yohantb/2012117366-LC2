ec70e00f8d88e6654987b4ec42feb6ed5b0d0091
Johan Tapia B <johan_tapia@usmp.pe>
2017-05-13 12:51:40 -0500

    Verificando Git

---
+++ 2012117366-SLN/2012117366-CON/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012117366_CON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
